237d335098805b97197a5fde102d3e987149a6ab
Matt Sroufe <matt.sroufe@powershop.co.nz>
2013-08-31 22:34:45 +1200

    add delegate 'customer_name' to transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,16 +1,17 @@
 class Transaction < ActiveRecord::Base
   attr_accessible :trans_type, :details, :particulars, :code, :reference, :amount, :date
   belongs_to :category
+  delegate :name, :to => :category, :prefix => true
 
   def self.import(file)
     CSV.foreach(file.path, :row_sep => :auto, :col_sep => ",") do |row|
-      Transaction.create! :trans_type => row[0],
-                          :details => row[1],
+      Transaction.create! :trans_type  => row[0],
+                          :details     => row[1],
                           :particulars => row[2],
-                          :code => row[3],
-                          :reference => row[4],
-                          :amount => row[5],
-                          :date => row[6]
+                          :code        => row[3],
+                          :reference   => row[4],
+                          :amount      => row[5],
+                          :date        => row[6]
     end
   end
 
