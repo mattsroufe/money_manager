a77974eccb96f69f687c7583c632d90a49249249
Matt Sroufe <mattsroufe@gmail.com>
2014-02-24 22:32:01 +1300

    changes budget view to show previous 12 months excluding current month

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,7 +2,7 @@ class BudgetsController < ApplicationController
   def show
     months = []
     for i in 0..11 do
-      months << (Time.now - i.month)
+      months << (Time.now - i.month - 1.month)
     end
     @months = months.reverse
   end
