e2b5bcca493a34f312b63bc48f320437898fcb48
Matt Sroufe <mattsroufe@gmail.com>
2025-06-12 09:00:51 +1200

    fix search

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,6 @@ class AccountsController < ApplicationController
 
   def show
     @account = Account.find(params[:id])
-    @txns = @account.txns.search(params[:search]).by_category(params[:category_id]).order("date DESC, amount DESC")
+    @txns = @account.txns.search(params[:search]).between(start_date: params[:start_date], end_date: params[:end_date]).by_category(params[:category_id]).order("date DESC, amount DESC")
   end
 end
